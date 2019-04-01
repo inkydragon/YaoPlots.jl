afd61157761d3cb1e6559e96a935ecbe8017a7ae
woclass <inkydragon@users.noreply.github.com>
2019-04-01 09:34:45 +0800

    add cross Cross Node, MultiQBitGate

--- Plot-Circuits.jl
+++ Plot-Circuits.jl
@@ -66,12 +66,16 @@ function sup(substr) :: AbstractString
 end
 
 function txtQbit(StartX::Int, lines::Int, init::Int=-1; dy=ΔY)
+    @assert lines >= 1 "lines >= 1"
+    @assert init in [-1, 0, 1] "Init state should be 0 or 1"
+    
     pos = Point(StartX, 0) + dy*(lines-1) 
     initState = init==-1 ? "?" : string(init)
     text = "|" * string(initState) * ">" *
             sub("Q"*string(lines))
     # In fact it support HTML code `&#10217;`
     # ⟩, U+27E9
+    
     # setfont("Helvetica", 24)
     settext(
         text, pos;
@@ -88,12 +92,12 @@ end
 =#
 
 """
-    gate(C, edge_len=20, txt="H")
+    gate(C::Point, edge_len=20, txt="H")
 
 Draw simple gates with box.
 """
-function gate(C, edge_len=20, txt="H")
-
+function gate(C::Point, txt="H", edge_len=20)
+    # TODO: support long gates
     # draw background
     sethue("white") 
     box(C, edge_len, edge_len, :fill)
@@ -113,6 +117,10 @@ function gate(C, edge_len=20, txt="H")
         valign = :middle
     )
 end
+gate(C::Point, txt::Char) = gate(C, string(txt))
+gate(C::Point, txt::Char, edge_len) = 
+    gate(C, string(txt), edge_len)
+
 
 """
     NotControlCircle(C::Point, size=:9, color=:"white")
@@ -150,6 +158,19 @@ function Not(C::Point, d=18)
     line(C+dY, C-dY, :stroke)
 end
 
+function Cross(C::Point, width=18)
+    Δ = width/2
+    dx = Point(Δ, 0)
+    dy = Point(0, Δ)
+    lt = C - dx + dy
+    rb = C + dx - dy
+    line(lt, rb, :stroke)
+    
+    rt = C + Δ
+    lb = C - Δ
+    line(rt, lb, :stroke)
+end
+
 
 """
     Toffoli(d::Dict, col::Int=1)
@@ -184,28 +205,81 @@ function Toffoli(d::Dict, col::Int=1)
 
 end
 
-function MeasureG(C)
+"""
+
+Draw general multi Qbits gate, you can draw single Qbit too.
+
+Input dict format:
+```julia
+d = Dict(
+    # Node
+    1 => "",    # "" or "C" for "Control Node"
+    2 => "C",
+    3 => "NC"   # "NC" for "Not Control Node"
+    6 => "X",   # "X" for "Cross Node"
+    
+    # Single Gate
+    4 => "N",   # "N" for "Not Gate"
+    7 => "SG:X" # start with "SG:", follow 1 char "X"
+                # for type "X" single Gate
+)
+```
+"""
+function MultiQBitGate(QBits::Dict, col::Int)
+    ks = collect(keys(QBits))
+    upper = P(col, minimum(ks))
+    lower = P(col, maximum(ks))
+    line(upper, lower, :stroke)
+    
+    for (y, typ) in QBits
+        if typ == "" || typ == "C"
+            ControlCircle(P(col, y))
+        elseif typ == "NC"
+            NotControlCircle(P(col, y))
+        elseif typ == "N"
+            Not(P(col, y))
+        elseif typ == "X"
+            Cross(P(col, y))
+        elseif startswith(typ, "SG:") && length(typ)==4
+            # TODO: support long gates
+            gateType = typ[end]
+            gate(P(col, y), gateType)
+        end
+    end
+end
+
+function MeasureG(C::Point)
     body
 end
 
 
 
 @svg begin
-nQbits = 5
+nQbits = 8
 nGates = 1
 rulers()
 init_qbit_line(nQbits)
 
 
-gate(P(), 20, "α")
-gate(P(2), 20, "H")
-gate(P(3), 20, "U")
-gate(P(4), 20, "-Z")
-gate(P(2,2), 20, "H")
+gate(P(), "α")
+gate(P(2), 'H')
+gate(P(3), "U")
+gate(P(4), "-Z")
+gate(P(2,2), "H")
 
 NotControlCircle(P(1,2))
 ControlCircle(P(1,3))
 Not(P(1,4))
+Cross(P(1,5))
 
 Toffoli(Dict(2=>"",3=>"",4=>"NC",5=>"T"), 3)
+MultiQBitGate(Dict(2=>"",3=>"",4=>"NC",5=>"N"), 4)
+MultiQBitGate(
+    Dict(
+        1=>"X",  2=>"",  3=>"",
+        4=>"NC", 5=>"N", 6=>"SG:H",
+        7=>"SG:X", 8=>"SG:ρ"
+    ), 
+    5
+)
 end
